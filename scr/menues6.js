4b471ad29aea928facbcc2d032157e42edaad2d2
RacoonRockstar <43373745+RacoonRockstar@users.noreply.github.com>
2018-11-26 19:16:24 +0300

    Add files via upload

---
+++ scr/menues6.js
@@ -0,0 +1,55 @@
+class Menu {
+  constructor(options) {
+      this.options = options
+  }
+
+  getElem() {
+      if (!this.elem) this.render();
+      return this.elem;
+  }
+
+  render() {
+      this.elem = document.createElement('div');
+      this.elem.className = "menu";
+
+      const titleElem = document.createElement('span');
+      this.elem.appendChild(titleElem);
+      titleElem.className = "title";
+      titleElem.textContent = this.options.title;
+
+      this.elem.onmousedown = () => false;
+
+      this.elem.onclick = event => {
+          if (event.target.closest('.title')) {
+              this.toggle();
+          }
+      }
+  }
+
+  renderItems() {
+      const items = this.options.items || [];
+      const list = document.createElement('ul');
+      items.forEach(item => {
+          const li = document.createElement('li');
+          li.textContent = item;
+          list.appendChild(li);
+      });
+      this.elem.appendChild(list);
+  }
+
+  open() {
+      if (!this.elem.querySelector('ul')) {
+          this.renderItems();
+      }
+      this.elem.classList.add('open');
+  };
+
+  close() {
+      this.elem.classList.remove('open');
+  };
+
+  toggle() {
+      if (this.elem.classList.contains('open')) this.close();
+      else this.open();
+  };
+}
